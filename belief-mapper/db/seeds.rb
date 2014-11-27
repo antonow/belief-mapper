25acb23c3b49f2bf035ae206d5b8b344d9c52d7d
Alexander Antonow & Patrick Grenning <pair+antonow.pgrennin@devbootcamp.com>
2014-11-27 11:23:49 -0600

    finished seeds

--- belief-mapper/db/seeds.rb
+++ belief-mapper/db/seeds.rb
@@ -4,10 +4,21 @@ belief = Belief.new
 
 File.open( 'phrontisteryisms.txt' ).each_with_index do |line, index|
   if index.even?
-    belief.name = line
+    belief.name = line.chomp
   else
-    belief.definition = line
+    belief.definition = line.chomp
     belief.save!
     belief = Belief.new
+  end
+end
+
+10.times do
+  User.create!(:email => Faker::Internet.email, :password => 'password', :password_confirmation => 'password')
+end
+
+User.all.each do |user|
+  rand(30..50).times do
+    belief = Belief.all.sample
+    user.beliefs << belief unless user.beliefs.include?(belief)
   end
 end
