75a22220d1f4b50a674b99db600ff2ce91a56528
Alexander Antonow & Patrick Grenning <pair+antonow.pgrennin@devbootcamp.com>
2014-11-29 12:38:04 -0600

    added saving of beliefs with 0 conviction

--- belief-mapper/app/controllers/user_beliefs_controller.rb
+++ belief-mapper/app/controllers/user_beliefs_controller.rb
@@ -1,18 +1,19 @@
 class UserBeliefsController < ApplicationController
   def create
     belief = Belief.find(params[:belief])
-    if create_belief?
-      user_belief = UserBelief.new(belief: belief, user: current_user, conviction: params[:conviction])
-      if user_belief.save!
-        create_connections_for(belief)
-        redirect 'pagethatdoesntexist'
-      end
+    # @count = 0
+    user_belief = UserBelief.new(belief: belief, user: current_user, conviction: params[:conviction])
+    if user_belief.save!
+      create_connections_for(belief)
+      # if @count = user.beliefs.count >= 3
+      redirect 'pagethatdoesntexist'
+      # end
     end
-    redirect_to root_path
+    render 'welcome/index'
   end
 
-  def create_belief?
-    params[:conviction].to_i > 5
-  end
+  # def create_belief?
+  #   params[:conviction].to_i > 5
+  # end
 end
 
