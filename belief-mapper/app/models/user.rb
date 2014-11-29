99e334c267bc332e5e5ca88ceb9d7d832368d6d7
Alexander Antonow & Patrick Grenning <pair+antonow.pgrennin@devbootcamp.com>
2014-11-29 17:32:32 -0600

    added coloring to beliefs held by current user

--- belief-mapper/app/models/user.rb
+++ belief-mapper/app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
   has_one  :demographic
   has_many :user_beliefs
   has_many :beliefs, through: :user_beliefs
+
+  def held_beliefs
+    return self.beliefs.where("conviction > ?", 5)
+  end
 end
