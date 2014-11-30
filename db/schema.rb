10a5a1596ff47ed205f82e224db3a4b16320dd6d
Peter Soderberg & Gina Krieb <pair+psoderberg.rockrgrrl@devbootcamp.com>
2014-11-29 20:37:28 -0600

    updated database with postgres adapter

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141128191950) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "beliefs", force: true do |t|
     t.string   "name"
     t.string   "definition"
@@ -50,8 +53,8 @@ ActiveRecord::Schema.define(version: 20141128191950) do
     t.datetime "updated_at"
   end
 
-  add_index "user_beliefs", ["belief_id"], name: "index_user_beliefs_on_belief_id"
-  add_index "user_beliefs", ["user_id"], name: "index_user_beliefs_on_user_id"
+  add_index "user_beliefs", ["belief_id"], name: "index_user_beliefs_on_belief_id", using: :btree
+  add_index "user_beliefs", ["user_id"], name: "index_user_beliefs_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -68,7 +71,7 @@ ActiveRecord::Schema.define(version: 20141128191950) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
