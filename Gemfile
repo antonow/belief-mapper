8d06a735918ddcb6b3bce078e667a60ec090630f
Alexander Antonow & Patrick Grenning <pair+antonow.pgrennin@devbootcamp.com>
2014-12-01 08:31:22 -0600

    removed searchkick gem

--- Gemfile
+++ Gemfile
@@ -32,8 +32,6 @@ gem 'faker'
 
 gem 'rails_12factor'
 
-gem "searchkick"
-
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/belief.rb
+++ app/models/belief.rb
@@ -4,6 +4,4 @@ class Belief < ActiveRecord::Base
   has_many :connections, foreign_key: 'belief_1_id'
   has_many :connections, foreign_key: 'belief_2_id'
 
-  searchkick text_start: [:name]
-
 end
